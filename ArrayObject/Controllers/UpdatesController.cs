661b039b591e3831fd184b75733f77517262c379
downhiill <downh068@gmail.com>
2024-12-26 12:08:19 +0200

    Добавьте файлы проекта.

---
+++ ArrayObject/Controllers/UpdatesController.cs
@@ -0,0 +1,44 @@
+﻿using ArrayObject.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArrayObject.Controllers
+{
+    [Route("api/updates")]
+    [ApiController]
+    public class UpdatesController : ControllerBase
+    {
+        private static readonly Dictionary<Guid, Update> Updates = new();
+
+        // Метод для обработки POST-запросов с массивом объектов Update
+        [HttpPost("complex")]
+        public IActionResult PostComplex([FromForm] Update[] updates)
+        {
+            if (updates != null && ModelState.IsValid)
+            {
+                foreach (var update in updates)
+                {
+                    update.Status = System.Net.WebUtility.HtmlEncode(update.Status);
+
+                    var id = Guid.NewGuid();
+                    Updates[id] = update;
+                }
+
+                return Ok("Updates processed successfully");
+            }
+
+            return BadRequest("Invalid data");
+        }
+
+        // Метод для обработки GET-запроса: api/updates/status/{id}
+        [HttpGet("status/{id}")]
+        public IActionResult Status(Guid id)
+        {
+            if (Updates.TryGetValue(id, out var update))
+            {
+                return Ok(update);
+            }
+            return NotFound("Update not found");
+        }
+    }
+
+}
